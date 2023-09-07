776cd2210473649717f999dbc83b258325e35716
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-07 16:24:17 +0300

    chore: fix timezone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, tzinfo
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,12 +18,13 @@ app.add_middleware(
 
 @app.get("/api")
 async def main(slack_name: str, track: str):
-    today = datetime.utcnow()
-
     return {
         "slack_name": slack_name,
-        "current_day": today.strftime("%A"),
-        "utc_time": today.utcnow(),
+        "current_day": datetime.now().strftime("%A"),
+        "utc_time": datetime.now(timezone.utc)
+        .replace(microsecond=0)
+        .isoformat()
+        .replace("+00:00", "Z"),
         "track": track,
         "github_file_url": "https://github.com/victor-ajayi/hngx-stage1/blob/main/main.py",
         "github_repo_url": "https://github.com/victor-ajayi/hngx-stage1",
